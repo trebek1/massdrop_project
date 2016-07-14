15c0c94b4cb789828424454eb5ec14cbf6c04027
trebek1 <trebek08@gmail.com>
2016-07-14 14:08:56 -0700

    setup routing so that the user is routed to ticket page when the query is made

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require("express"),
     ejs = require("ejs"),
     bodyParser = require("body-parser"),
     methodOverride = require("method-override"),
-    app = express(); 
+    app = express(),
+    request = require('request'),
+    db = require('./models'); 
 
 // Set the view engine to be "EJS"
 app.set('view engine', 'ejs');
@@ -16,7 +18,31 @@ app.use(methodOverride('_method'));
 
 // Let's add some routes here together:
 app.get('/', function(req, res){
-	res.render('index');
+	res.redirect('/tickets');
+});
+
+app.get('/tickets', function(req, res){
+	res.render('index'); 
+})
+
+app.post('/tickets', function(req,res){
+
+	var url = req.body.address;
+
+	console.log("this is url ", url);
+	request(url, function(err, response, body) {
+		
+		db.Record.create({
+			address: url, 
+			data: body
+		}).then(function(record){
+			res.render('ticket', {id: record.id});	
+		});
+		
+		});
+
+	
+	
 });
 
 // Start the server on port 3000
